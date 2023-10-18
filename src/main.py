2f164d93d1766b922bb6c28e53e0294f0738d588
Daniel Jorge Acosta <alu0101239187@ull.edu.es>
2023-10-18 12:23:37 +0100

    diferencia con la media

--- src/main.py
+++ src/main.py
@@ -45,7 +45,23 @@ def distancia_coseno(u_values, v_values):
 
     return round(numerator / (math.sqrt(u_denominator) * math.sqrt(v_denominator)), 10)
 
-def prediccion_simple(similitary, neighbours):
+def distancia_euclidea(u_values, v_values):
+    # Elimina las columnas con incognita
+    u_filtered_values = []
+    v_filtered_values = []
+    for i in range(len(u_values)):
+        if u_values[i] >= 0 and v_values[i] >= 0:
+            u_filtered_values.append(u_values[i])
+            v_filtered_values.append(v_values[i])
+
+    # Calcula la distancia euclidea
+    summatory = 0
+    for i in range(len(u_filtered_values)):
+        summatory += math.pow(u_filtered_values[i] - v_filtered_values[i], 2)
+
+    return round(1 / math.sqrt(summatory), 10)
+
+def prediccion_simple(similarity, neighbours):
     numerator = 0
     denominator = 0
     for i in range(neighbours):
@@ -53,6 +69,29 @@ def prediccion_simple(similitary, neighbours):
         denominator += abs(similarity[i][1])
 
     return round(numerator/denominator, 3)
+
+def diferencia_con_media(similarity, neighbours, matrix, u_index):
+    u_filtered_values = []
+    v_filtered_values = []
+    numerator = 0
+    denominator = 0
+
+    for i in range(neighbours):
+        # Elimina las columnas con incognita
+        for j in range(len(matrix[u_index])):
+            if matrix[u_index][j] >= 0 and matrix[similarity[i][0]][j] >= 0:
+                u_filtered_values.append(matrix[u_index][j])
+                v_filtered_values.append(matrix[similarity[i][0]][j])
+
+        # Calcula las medias
+        u_average = sum(u_filtered_values) / len(u_filtered_values)
+        v_average = sum(v_filtered_values) / len(v_filtered_values)
+
+        # Calcula la diferencia de la media
+        numerator += (similarity[i][1] * (similarity[i][2] - v_average))
+        denominator += abs(similarity[i][1])
+
+    return round((u_average + numerator/denominator), 3)
 
 parser = argparse.ArgumentParser(prog='ProgramName', description='What the program does', epilog='Text at the bottom of help')
 parser.add_argument('-f', '--filename', type=str, required=True, help="Fichero de entrada")
@@ -74,7 +113,7 @@ for i in range(len(matrix)):
             similarity = []
             for k in range(len(matrix)):
                 if i != k:
-                    similarity.append((k, distancia_coseno(matrix[i], matrix[k]), matrix[k][j]))
+                    similarity.append((k, distancia_euclidea(matrix[i], matrix[k]), matrix[k][j]))
             similarity.sort(key=lambda x: x[1], reverse=True)
             print(similarity)
 
@@ -83,5 +122,5 @@ for i in range(len(matrix)):
             for k in range(neighbours):
                 print(similarity[k][0])
 
-            matrix[i][j] = prediccion_simple(similarity, neighbours)
+            matrix[i][j] = diferencia_con_media(similarity, neighbours, matrix, i)
             print(matrix)
