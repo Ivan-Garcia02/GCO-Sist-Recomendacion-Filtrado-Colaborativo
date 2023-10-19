a2ed4d8b0be632e0cacdcef35824a8f9c2577fff
Daniel Jorge Acosta <alu0101239187@ull.edu.es>
2023-10-19 10:13:10 +0100

    Añadido file_reader a README.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# GCO-Sist-Recomendacion
+# Sistemas de recomendación. Métodos de filtrado colaborativo
+
+### Iván García González
+
+### Daniel Jorge Acosta
+
+## Instrucciones de instalación
+
+Python
+
+## Descripción del código
+
+El código del proyecto se encuentra en dividido en varios ficheros.
+
+### file_reader
+
+Contiene la función `read_file`, encargada de crear un array de dos dimensiones a partir del contenido del fichero de entrada. Además, controla que los valores de este array se encuentren entre el mínimo y el máximo establecidos en el fichero y que todas sus filas tengan la misma longitud. La matriz devuelta tendrá sustituidas las incógnitas por el valor -1.0 y sus valores se modificarán para que se sitúen en el intervalo [0.0, max - min], en lugar de [min, max], con el objetivo de facilitar el trabajo con esta.
+
+## Ejemplo de uso
--- src/file_reader.py
+++ src/file_reader.py
@@ -1,23 +1,30 @@
 def read_file(file_path):
     matrix = []
+
     with open(file_path) as file:
+        # Lee el valor mínimo y el valor máximo del fichero
         min_value = float(file.readline())
         max_value = float(file.readline())
         if max_value <= min_value:
             print("El valor mínimo debe ser menor al valor máximo")
             return
 
+        # Lee la matriz del fichero
         n_columns = 0
         while True:
             line = file.readline()
             if not line:
                 break
+
+            # Controla que el número de columnas de la fila sea el mismo
             splitted_line = line.split()
             if n_columns == 0:
                 n_columns = len(splitted_line)
             if len(splitted_line) != n_columns:
                 print("Todas las filas de la matriz deben tener el mismo número de columnas")
                 return
+
+            # Añade los valores a la matriz final 
             values = []
             for value in splitted_line:
                 if value == '-':
@@ -33,4 +40,5 @@ def read_file(file_path):
                     values.append(value - min_value)
             matrix.append(values)
     file.close()
+
     return matrix, min_value, max_value
